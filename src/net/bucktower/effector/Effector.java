6e393e6d8786b8422d2fc11bcdad12f4dca07a3f
Buck <bucktower14@gmail.com>
2014-11-24 00:03:22 -0600

    Initial Commit

---
+++ src/net/bucktower/effector/Effector.java
@@ -0,0 +1,17 @@
+package net.bucktower.effector;
+
+import processing.core.PApplet;
+
+
+public class Effector extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { net.bucktower.effector.Effector.class.getName() });
+	}
+}
