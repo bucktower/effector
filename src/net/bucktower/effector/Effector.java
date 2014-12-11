801b63028abaac54c0c65e7be3379fd443a81b48
Buck <bucktower14@gmail.com>
2014-12-10 21:18:42 -0600

    created 4 photothings

--- src/net/bucktower/effector/Effector.java
+++ src/net/bucktower/effector/Effector.java
@@ -9,7 +9,12 @@ public class Effector extends PApplet {
 	
 	Capture cam;
 	ArrayList<Integer> myColors;
+	ArrayList<Integer> myBWColors;
+	ArrayList<Integer> myRGBColors;
 	ImageEditor view;
+	ImageEditor view2;
+	ImageEditor view3;
+	ImageEditor view4;
 	
 	int camWidth = 640;
 	int camHeight = 360;
@@ -20,7 +25,7 @@ public class Effector extends PApplet {
 		
 	  cam = new Capture(this, camWidth,camHeight,15);
 	  frameRate(35);
-	  size(camWidth,camHeight);
+	  size(camWidth*2,camHeight*2);
 	  cam.start();
 	  // this next part waits for the camera to warm up.
 	  int i=0;
@@ -41,13 +46,28 @@ public class Effector extends PApplet {
 	  myColors.add(color(100,0,200)); // purple
 	  myColors.add(color(200,200,200)); // white
 	  //myColors.add(color(0,0,0));  // black
+	  
+	  myBWColors = new ArrayList<Integer>();
+	  myBWColors.add(color(200,200,200)); // white
+	  myBWColors.add(color(0,0,0));  // black
+	  
+	  myRGBColors = new ArrayList<Integer>();
+	  myRGBColors.add(color(200,0,0)); // red
+	  myRGBColors.add(color(0,200,0)); // green
+	  myRGBColors.add(color(0,0,200));  // blue
 	}
 
 	public void draw()
 	{
 	  cam.read();
 	  view = new ImageEditor(cam);
+	  view2 = new ImageEditor(cam);
+	  view3 = new ImageEditor(cam);
+	  view4 = new ImageEditor(cam);
 	  view.startEditing();
+	  view2.startEditing();
+	  view3.startEditing();
+	  view4.startEditing();
 	    for (int y=0; y<view.height(); y++)
 	      for (int x=0; x<view.width(); x++)
 	      {
@@ -70,9 +90,81 @@ public class Effector extends PApplet {
 	          // the pixel with that color.
 	          view.setColorAt(closestColor,x,y);
 	      }
+	    for (int y=0; y<view.height(); y++)
+		      for (int x=0; x<view.width(); x++)
+		      {
+		          // find which color in the list is the closest to the color at (x,y).
+		          int closestColor = color(0,0,0); // black... but we can do better.
+		          
+		          float maxDistSquared = 0;  // worse than any possible color distance....
+		          
+		          for (int c: myColors)
+		          {
+		             // how far is the color at (x,y) from this item in the list?
+		             float colDist = colorDistanceSquared(view2.colorAt(x,y),c);
+		             if (colDist>maxDistSquared)
+		             {
+		                 maxDistSquared = colDist;
+		                 closestColor = c;
+		             } 
+		          }
+		          // now that we've found which color is closest to the original pixel, we'll replace
+		          // the pixel with that color.
+		          view2.setColorAt(closestColor,x,y);
+		      }
+	    for (int y=0; y<view.height(); y++)
+		      for (int x=0; x<view.width(); x++)
+		      {
+		          // find which color in the list is the closest to the color at (x,y).
+		          int closestColor = color(0,0,0); // black... but we can do better.
+		          
+		          float minDistSquared = 3*pow(256,2);  // worse than any possible color distance....
+		          
+		          for (int c: myBWColors)
+		          {
+		             // how far is the color at (x,y) from this item in the list?
+		             float colDist = colorDistanceSquared(view3.colorAt(x,y),c);
+		             if (colDist<minDistSquared)
+		             {
+		                 minDistSquared = colDist;
+		                 closestColor = c;
+		             } 
+		          }
+		          // now that we've found which color is closest to the original pixel, we'll replace
+		          // the pixel with that color.
+		          view3.setColorAt(closestColor,x,y);
+		      }
+	    for (int y=0; y<view.height(); y++)
+		      for (int x=0; x<view.width(); x++)
+		      {
+		          // find which color in the list is the closest to the color at (x,y).
+		          int closestColor = color(0,0,0); // black... but we can do better.
+		          
+		          float minDistSquared = 3*pow(256,2);  // worse than any possible color distance....
+		          
+		          for (int c: myRGBColors)
+		          {
+		             // how far is the color at (x,y) from this item in the list?
+		             float colDist = colorDistanceSquared(c,view4.colorAt(x,y));
+		             if (colDist<minDistSquared)
+		             {
+		                 minDistSquared = colDist;
+		                 closestColor = c;
+		             } 
+		          }
+		          // now that we've found which color is closest to the original pixel, we'll replace
+		          // the pixel with that color.
+		          view4.setColorAt(closestColor,x,y);
+		      }
 	  view.stopEditing();
+	  view2.stopEditing();
+	  view3.stopEditing();
+	  view4.stopEditing();
 	  view.drawAt(0,0);
-	   
+	  view2.drawAt(width/2, 0);
+	  view3.drawAt(0, height/2);
+	  view4.drawAt(width/2, height/2);
+	  noLoop();
 	}
 
 	float colorDistanceSquared(int c1, int c2)
